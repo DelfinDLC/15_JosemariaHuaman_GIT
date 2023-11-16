1d819c60716839726931a560e0c473754fec3bd8
DelfinDLC <138272140+DelfinDLC@users.noreply.github.com>
2023-11-16 12:06:15 -0500

    Add files via upload

---
+++ 15_JosemariaHuaman/src/S11_15/Profesor_15.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S11_09;
+
+/**
+ *
+ * @author Lab18
+ */
+public class Profesor_09 {
+    String curso;
+    
+}
---
+++ 15_JosemariaHuaman/src/S11_15/demo_15.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S11_09;
+
+/**
+ *
+ * @author Lab18
+ */
+public class demo_09 {
+
+    public demo_09(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    int a;
+    String b;
+}
