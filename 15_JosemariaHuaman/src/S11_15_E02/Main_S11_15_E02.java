84dc8c276a4e9454a2381bf567f55ffa5cabdf70
DelfinDLC <138272140+DelfinDLC@users.noreply.github.com>
2023-11-16 12:10:38 -0500

    Main_S11_15_E02.java

--- 15_JosemariaHuaman/src/S11_15_E02/Main_S11_15_E02.java
+++ 15_JosemariaHuaman/src/S11_15_E02/Main_S11_15_E02.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package S11_09_E02;
+package S11_15_E02;
 
 /**
  *
